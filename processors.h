8897710a86821162de8efed3c68e9d3f22d36da6
Working <Working@UserPC>
2020-05-11 11:25:52 +0300

    add function in header file

--- processors.h
+++ processors.h
@@ -19,14 +19,23 @@ typedef struct my_struct3
 }stack;
 */
 
+// функция первого процессора вернёт 0 при нехватке памяти
+int processor_1(Q *Queue, int *timer, int *iteration, int time_to_complete /*параметры для функций*/);
 
+// функция первого процессора вернёт 0 при нехватке памяти
+int processor_2(Q *Queue, int *timer, int *iteration, /**/int time_to_complete /**/ /*параметры для функций*/);
 
+// функция первого процессора вернёт 0 при нехватке памяти
+int processor_3(Q *Queue, int *timer, int *iteration, /**/int time_to_complete /**/ /*параметры для функций*/);
 
+// распределитель задач (вернёт 0 в случае выхода из программы(при нажатии ESC))
+int distributer(Q Queue[], char c);
 
+// подготовка для процессоров
+int preparation(Q Queue[], int timer[], int iteration[]);
 
-
-
-
+// завершение работы - очистка памяти
+void the_end(Q Queue[]);
 
 // процесс работы
 int process();
